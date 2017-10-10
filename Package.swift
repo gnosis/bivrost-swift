4bdf6e0fa3b4ca72966aa4cd7701d1f9d3f4a2a0
Luis Reisewitz <reisewitz@me.com>
2017-10-10 13:54:20 +0200

    Some CLI stuff

--- Package.swift
+++ Package.swift
@@ -8,18 +8,22 @@ let package = Package(
         .library(name: "BivrostBase", targets: ["BivrostBase"])
     ],
     dependencies: [
+        // CryptoSwift fork including Keccak in master
         .package(url: "https://github.com/zweigraf/CryptoSwift.git", .revision("fdd115318528c9c25a06c0a89553f71bde7233da")),
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.0.2")),
         .package(url: "https://github.com/Quick/Quick.git", .revision("c498edf4aabb694a5b8a861ec3d69f0c5ab57d9e")),
-        .package(url: "https://github.com/kylef/Commander.git", .revision("e0cbee1bd73778c1076c675eaf660e97d09f3b32"))
+        .package(url: "https://github.com/kylef/Commander.git", .revision("e0cbee1bd73778c1076c675eaf660e97d09f3b32")),
+        // PathKit fork supporting SPM4
+        .package(url: "https://github.com/PoissonBallon/PathKit.git", .revision("0b109bc868e9a12bc5f23e9d61ec937c75900c40"))
     ],
     targets: [
         .target(
             name: "Bivrost",
             dependencies: [
                 "BivrostBase",
-                "Commander"
+                "Commander",
+                "PathKit"
             ]
         ),
         .target(
--- Sources/Bivrost/main.swift
+++ Sources/Bivrost/main.swift
@@ -9,6 +9,7 @@
 import Commander
 import BivrostBase
 import Foundation
+import PathKit
 
 func jsonArray(from path: String) throws -> [[String: Any]] {
     let url = URL(fileURLWithPath: path)
@@ -16,16 +17,27 @@ func jsonArray(from path: String) throws -> [[String: Any]] {
     return try! JSONSerialization.jsonObject(with: data, options: []) as! [[String: Any]]
 }
 
-let group = Group {
-    $0.command("parse", description: "Parses ABI files and outputs representation") { (path: String) in
-        print("Parsing \(path)")
-        let json = try jsonArray(from: path)
-        print(try JsonParser.parseContract(from: json))
-    }
-    $0.command("generate", description: "Generates necessary Solidity files for Swift") { (path: String) in
-        print("Generate: Dummy command in path \(path)")
+func parse(path: String) throws -> String {
+    let json = try jsonArray(from: path)
+    return try JsonParser.parseContract(from: json)
+}
+
+func parsePattern(pattern: String) throws -> [String] {
+    let paths = Path.glob(pattern)
+    return try paths.map {
+        try parse(path: $0.absolute().string)
     }
 }
 
-group.run()
+let group = Group {
+    let parseCommand = command(
+        Argument<String>("pattern",
+                         description: "Pattern for json files to be parsed. Needs to be escaped with quotes to prevent the shell from expanding it.")) {
+                            print(try parsePattern(pattern: $0))
+    }
+    $0.addCommand("parse",
+                  "Parses ABI files and outputs internal representation",
+                  parseCommand)
+}
 
+group.run()
