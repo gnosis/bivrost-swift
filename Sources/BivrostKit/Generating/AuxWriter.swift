f96934502078254eb8d0888152b6260cdcde2974
Luis Reisewitz <reisewitz@me.com>
2017-10-16 11:49:14 +0200

    use working dir / Resources as base for copying. works with mint & spm

--- Sources/BivrostKit/Generating/AuxWriter.swift
+++ Sources/BivrostKit/Generating/AuxWriter.swift
@@ -9,15 +9,9 @@ import PathKit
 
 struct AuxWriter {
     static func writeAuxiliaryFiles(to outputFolder: String) throws {
-        try print(Path("./").children())
-        
-//        let resources = Bivrosthelper()
-//        // Write resources to file system
-//        try resources.export(to: outputFolder)
-//        // Move one level up (as FakeBundle export adds an intermediate folder named `BivrostHelper`
-//        let outputFolder = Path(outputFolder)
-//        let fullPath = outputFolder + resources.filename
-//        try fullPath.children().forEach { try $0.move(outputFolder + $0.lastComponent) }
-//        try fullPath.delete()
+        try FileTool.create(path: outputFolder)
+        let outputFolder = Path(outputFolder)
+        let resources = Path("./Resources")
+        try resources.children().forEach { try $0.copy(outputFolder + $0.lastComponent) }
     }
 }
