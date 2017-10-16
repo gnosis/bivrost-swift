b871390dc08814c4f44c7e97566035a23f4a84f8
Luis Reisewitz <reisewitz@me.com>
2017-10-16 10:55:03 +0200

    test mint

--- Sources/BivrostKit/Generating/AuxWriter.swift
+++ Sources/BivrostKit/Generating/AuxWriter.swift
@@ -9,13 +9,15 @@ import PathKit
 
 struct AuxWriter {
     static func writeAuxiliaryFiles(to outputFolder: String) throws {
-        let resources = Bivrosthelper()
-        // Write resources to file system
-        try resources.export(to: outputFolder)
-        // Move one level up (as FakeBundle export adds an intermediate folder named `BivrostHelper`
-        let outputFolder = Path(outputFolder)
-        let fullPath = outputFolder + resources.filename
-        try fullPath.children().forEach { try $0.move(outputFolder + $0.lastComponent) }
-        try fullPath.delete()
+        try print(Path("./").children())
+        
+//        let resources = Bivrosthelper()
+//        // Write resources to file system
+//        try resources.export(to: outputFolder)
+//        // Move one level up (as FakeBundle export adds an intermediate folder named `BivrostHelper`
+//        let outputFolder = Path(outputFolder)
+//        let fullPath = outputFolder + resources.filename
+//        try fullPath.children().forEach { try $0.move(outputFolder + $0.lastComponent) }
+//        try fullPath.delete()
     }
 }
