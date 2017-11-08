dc856694260b056b038014ad80027a4c7b39ad4c
Luis Reisewitz <reisewitz@me.com>
2017-11-08 14:29:08 +0100

    fix warning

--- Sources/BivrostKit/Parsing/Solc0_4ContractParser.swift
+++ Sources/BivrostKit/Parsing/Solc0_4ContractParser.swift
@@ -7,9 +7,9 @@
 
 struct Solc0_4ContractParser: ContractParser {
     static func parseContract(from json: JsonDict) throws -> Contract {
-        guard let contractsDict = json[.contracts] as? JsonDict else {
-            throw ParsingError.contractsDictInvalid
-        }
+//        guard let contractsDict = json[.contracts] as? JsonDict else {
+//            throw ParsingError.contractsDictInvalid
+//        }
 
         // FIXME: Solc0.4 combined json includes multiple contracts in one file.
         // FIXME: This is not compatible with our current architecture of input files.
